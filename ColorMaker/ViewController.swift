4c3b7380108471375809d85c1361d4c9cbfcb9d9
Rohit Das <60841345+Rohit2001Das@users.noreply.github.com>
2021-11-10 11:32:55 +0530

    added all the files

---
+++ ColorMaker/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  ColorMaker
+
+import UIKit
+
+// MARK: - ViewController: UIViewController
+
+class ViewController: UIViewController {
+    
+    // MARK: Outlets
+    
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var redControl: UISlider!
+    @IBOutlet weak var greenControl: UISlider!
+    @IBOutlet weak var blueControl: UISlider!
+
+    // MARK: Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    // MARK: Actions
+    
+    @IBAction func changeColorComponent(_ sender: UISlider) {
+        
+        let r: CGFloat = CGFloat(self.redControl.value)
+        let g: CGFloat = CGFloat(self.greenControl.value)
+        let b: CGFloat = CGFloat(self.blueControl.value)
+                
+        colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
+    }
+}
+
